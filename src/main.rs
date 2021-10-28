df4132733fbb17e9acf389f440aa07a6c515b440
Maxim Zhukov <mussitantesmortem@gmail.com>
2021-10-28 11:54:28 +0300

    fizzbuzz: refactor
    Убрал лишние аллокации памяти заменив массив на итератор, который
    принимает enum FizzBuzz с имплементированным Display
    
    Signed-off-by: Maxim Zhukov <mussitantesmortem@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
-use std::vec::Vec;
+use std::fmt;
 
 fn main() {
     const COUNT: usize = 100;
-    let mut result = Vec::with_capacity(COUNT);
-    for i in 1..=COUNT {
-        let s = match (i % 3, i % 5) {
-            (0, 0) => String::from("FizzBuzz"),
-            (0, _) => String::from("Fizz"),
-            (_, 0) => String::from("Buzz"),
-            (_, _) => format!("{}", i),
-        };
-        result.push(s);
-    }
-    print_result(&result);
+
+    print_result(fizzbuzz_iter(COUNT));
 }
 
-fn print_result(result: &[String]) {
+enum FizzBuzz {
+    Fizz,
+    Buzz,
+    FizzBuzz,
+    NoMatches(usize),
+}
+
+impl From<usize> for FizzBuzz {
+    fn from(i: usize) -> FizzBuzz {
+        match (i % 3, i % 5) {
+            (0, 0) => FizzBuzz::FizzBuzz,
+            (0, _) => FizzBuzz::Fizz,
+            (_, 0) => FizzBuzz::Buzz,
+            (_, _) => FizzBuzz::NoMatches(i),
+        }
+    }
+}
+
+impl fmt::Display for FizzBuzz {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FizzBuzz::FizzBuzz => write!(f, "FizzBuzz"),
+            FizzBuzz::Fizz => write!(f, "Fizz"),
+            FizzBuzz::Buzz => write!(f, "Buzz"),
+            FizzBuzz::NoMatches(i) => write!(f, "{}", i),
+        }
+    }
+}
+
+fn fizzbuzz_iter(count: usize) -> impl Iterator<Item = FizzBuzz> {
+    (1..=count).map(FizzBuzz::from)
+}
+
+fn print_result(result: impl Iterator<Item = FizzBuzz>) {
     for r in result {
         println!("{}", r)
     }
